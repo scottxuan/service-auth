f37b470086a0d2f0f7a83d7ea80b87a6db71deca
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-07 17:35:23 +0800

    INIT

--- src/main/java/com/service/auth/AuthApplication.java
+++ src/main/java/com/service/auth/AuthApplication.java
@@ -2,6 +2,8 @@ package com.service.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -12,9 +14,14 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan(basePackages = {"com.service.auth.mapper"})
-@ComponentScan(basePackages = {"com.scottxuan","com.module.auth","com.service.auth"})
-public class AuthApplication {
+@ComponentScan(basePackages = {"com.scottxuan","com.service.auth"})
+public class AuthApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AuthApplication.class);
     }
 }
