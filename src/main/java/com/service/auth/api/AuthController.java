c929e56e16bc3bcccf91568b3ded8da38d49166f
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-15 17:28:11 +0800

    auth

--- src/main/java/com/service/auth/api/AuthController.java
+++ src/main/java/com/service/auth/api/AuthController.java
@@ -1,5 +1,9 @@
 package com.service.auth.api;
 
+import com.module.common.wechat.condition.WxCode2Session;
+import com.module.common.wechat.core.WxClient;
+import com.module.common.wechat.request.WxCode2SessionRequest;
+import com.module.common.wechat.response.WxCode2SessionResponse;
 import com.scottxuan.web.base.BaseController;
 import com.scottxuan.web.result.ResultDto;
 import io.swagger.annotations.Api;
@@ -10,9 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 /**
  * @author : scottxuan
@@ -25,10 +26,10 @@ public class AuthController extends BaseController {
     @ApiOperation("001--用户登录")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "storeId", value = "店铺id", required = true, dataType = "string", paramType = "query"),
-            @ApiImplicitParam(name = "id", value = "id", required = true, dataType = "int", paramType = "query"),
+            @ApiImplicitParam(name = "id", value = "id", required = true, example = "1", dataType = "int", paramType = "query"),
             @ApiImplicitParam(name = "code", value = "微信登录code", required = true, dataType = "string", paramType = "query")})
     @GetMapping
-    public ResultDto login(@RequestParam @NotBlank String storeId, @RequestParam @NotNull Integer id, @RequestParam @NotBlank String code) {
+    public ResultDto login(@RequestParam String storeId, @RequestParam Integer id, @RequestParam String code) {
 //        String appId = "wxd7285bcf42c3836e";
 //        String secret = "28f0e6ca22962a20866127e134e5f835";
 //        WxCode2Session wxCode2Session = new WxCode2Session(appId, secret, code);
