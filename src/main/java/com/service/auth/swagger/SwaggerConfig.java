901b987bd98577f4693ab8d5d6c13e7c624ac9c3
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-21 11:12:05 +0800

    SwaggerUI增强

--- src/main/java/com/service/auth/swagger/SwaggerConfig.java
+++ src/main/java/com/service/auth/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.service.auth.swagger;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.scottxuan.web.swagger.AbstractSwaggerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
 public class SwaggerConfig extends AbstractSwaggerConfig {
 
     private static final String BASEPACKAGE = "com.service.auth.api";
